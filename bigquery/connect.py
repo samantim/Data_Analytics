e699efd19eaed12ca59f7feb04db04f108307f4b
samantim <saman.teymouri@gmail.com>
2025-03-15 17:28:41 +0100

    first insert

--- bigquery/connect.py
+++ bigquery/connect.py
@@ -1,6 +1,27 @@
 from google.cloud import bigquery
+from faker import Faker
+import datetime
 
-client = bigquery.Client()
+def connect_bigquery():
+    client = bigquery.Client("secret-compass-453715-h4")
+    return client
 
-result = client.query("SELECT * from `secret-compass-453715-h4.ecommerce.cities`")
-print(result)
+def populate_cities(bigquert_connection):
+    query = bigquert_connection.query("INSERT INTO `secret-compass-453715-h4.ecommerce.cities` (city_id, name, created_at) VALUES (1, 'Basel', '2023-10-01 12:00:00')")
+    query = bigquert_connection.query("INSERT INTO `secret-compass-453715-h4.ecommerce.cities` (city_id, name, created_at) VALUES (2, 'Berlin', '2023-10-01 12:00:00')")
+    query.result()  # Wait for the query to finish
+
+
+def show_cities(bigquert_connection):
+    query = bigquert_connection.query("SELECT * from `secret-compass-453715-h4.ecommerce.cities`")
+
+    df = query.to_dataframe()
+    print(df)
+
+def main():
+    client = connect_bigquery()
+    populate_cities(client)
+    show_cities(client)
+
+if __name__ == "__main__":
+    main()
