30dfd2983d936f4678cd04e4f919fe8833e50fe0
samantim <saman.teymouri@gmail.com>
2025-04-01 17:32:16 +0200

    add crypto convert to actions

--- chatbot-rasa/actions/actions.py
+++ chatbot-rasa/actions/actions.py
@@ -16,18 +16,46 @@ class ActionConvertCurrency(Action):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        src_currency = tracker.get_slot("src_currency")
-        des_currency = tracker.get_slot("des_currency")
+        src_currency = str(tracker.get_slot("src_currency")).upper()
+        des_currency = str(tracker.get_slot("des_currency")).upper()
         amount = tracker.get_slot("amount")
 
+        conversion_found = False
+        src_crypto = False
+        # First check if the result is available in the currency convertion (In case both are currencies not cryptos)
         api_key = "e61686068c8b2aff51c8519f"
         response = requests.get(f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{src_currency}")
         data = response.json()
-        conversion_rate = data["conversion_rates"][des_currency]
+        if "conversion_rates" in data:
+            if des_currency in data["conversion_rates"]:
+                conversion_rate = data["conversion_rates"][des_currency]
+                conversion_found = True
+            else:
+                src_crypto = False
+        else:
+            src_crypto = True
+
+        # This means that on of the src or des is a crpto not a ordinary cusrrency
+        if not conversion_found:
+            api_key = "72c440762a98d30f084591df24b14332"
+            response = requests.get("http://api.coinlayer.com/api/live", params={
+                "access_key" : api_key,
+            })
+            data = response.json()
+            if src_crypto:
+                crypto_currency = src_currency
+                des_currency = "USD"
+            else:
+                crypto_currency = des_currency
+                src_currency = "USD"
+            conversion_rate = data["rates"][crypto_currency]
 
         converted_amount = amount * conversion_rate
         dispatcher.utter_message(
             f"The conversion rate from {src_currency} to {des_currency} is {conversion_rate}. {amount} {src_currency} equals {converted_amount} {des_currency}."
         )
-        # return [SlotSet("conversion_rate", conversion_rate), SlotSet("converted_amount", converted_amount)]
         return []
+    
+
+
+
